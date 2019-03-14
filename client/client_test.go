03f6fed580088ad603843f5dbdb52056e7a282cd
Suzanne Richman <srichm@ext.uber.com>
2019-03-13 21:34:31 -0700

    got tests started and create test works, but doesn't need to ask Scott about this more I think.

---
+++ client/client_test.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	
+	"CacheCLI/server"
+)
+
+func TestNewCacheClient(t *testing.T) {
+	t.Run("it creates new cache", func(t *testing.T) {
+		testCache := NewCacheClient()
+		assert.NotNil(t, testCache)
+	})
+}
+
+func TestCacheClient_Create(t *testing.T) {
+	clientCache := NewCacheClient()
+	//client := &http.Client{}
+	go server.StartServer("8080")
+	
+	t.Run("create request works", func(t *testing.T){
+		err := clientCache.Create("foo", "bar")
+		require.Error(t, err)
+		
+		rr := httptest.NewRecorder()
+		//status code passed back is correct too
+		assert.Equal(t, rr.Code, http.StatusOK )
+	})
+}
